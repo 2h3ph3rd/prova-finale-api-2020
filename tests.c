9462d520408f6d439e03ec3a68f194c7cf2e5ca5
Francesco Pastore <francesco0599@hotmail.it>
2020-07-21 10:30:45 +0200

    Added new test multipleChangeAndPrint

--- tests.c
+++ tests.c
@@ -17,6 +17,7 @@ int main()
     char *folders[100] = {
 		"/onlyQuit",
         "/changeAndPrint",
+		"/multipleChangeAndPrint",
 		"/prof",
 		"/publicTests/writeonly1",
 		"/publicTests/writeonly2"
